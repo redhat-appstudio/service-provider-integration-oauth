f18b59d72e57f8369947efb1d7de3a1f8b0491eb
Max Shaposhnik <mshaposh@redhat.com>
2022-02-07 11:30:17 +0200

    Test fixup
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

--- controllers/common_test.go
+++ controllers/common_test.go
@@ -198,7 +198,7 @@ var _ = Describe("Controller", func() {
 
 			g.Callback(ctx, res, req)
 
-			Expect(res.Code).To(Equal(http.StatusOK))
+			Expect(res.Code).To(Equal(http.StatusFound))
 			Expect(serviceProviderReached).To(BeTrue())
 		})
 	})

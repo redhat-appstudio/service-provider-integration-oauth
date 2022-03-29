d0384304abbb594c7b4162998cf76e92b2bd62b6
Lukas Krejci <lkrejci@redhat.com>
2022-03-29 12:11:20 +0200

    Configure the logger to output everything to stdout and output an info message at http server start.

--- main.go
+++ main.go
@@ -77,15 +77,22 @@ func main() {
 	args := cliArgs{}
 	arg.MustParse(&args)
 
-	var logger *zap.Logger
+	var loggerConfig zap.Config
 	if args.DevMode {
-		logger, _ = zap.NewDevelopment()
+		loggerConfig = zap.NewDevelopmentConfig()
 	} else {
-		logger, _ = zap.NewProduction()
+		loggerConfig = zap.NewProductionConfig()
 	}
-	if logger != nil {
-		zap.ReplaceGlobals(logger)
+	loggerConfig.OutputPaths = []string{"stdout"}
+	loggerConfig.ErrorOutputPaths = []string{"stdout"}
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to initialize logging: %s", err.Error())
+		os.Exit(1)
 	}
+	defer logger.Sync()
+	undo := zap.ReplaceGlobals(logger)
+	defer undo()
 
 	cfg, err := config.LoadFrom(args.ConfigFile)
 	if err != nil {
@@ -125,6 +132,7 @@ func start(cfg config.Configuration, port int, kubeConfig *rest.Config) {
 		})).Methods("GET")
 	}
 
+	zap.L().Info("Starting the server", zap.Int("port", port))
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 	if err != nil {
 		zap.L().Error("failed to start the HTTP server", zap.Error(err))
